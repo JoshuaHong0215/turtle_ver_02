3b6de9e0edcbfbe6709d65ffcaf5c033869859e6
JoshuaHong0215 <sebin5736@gmail.com>
2025-07-22 14:58:40 +0900

    agent 추가

--- Pyamaze_ver_01.py
+++ Pyamaze_ver_01.py
@@ -1,7 +1,11 @@
 # turtle_ver_02
 
-from pyamaze import maze
+from pyamaze import maze,COLOR,agent
 
-m = maze()
-m.CreateMaze()
+m = maze(10,10)
+m.CreateMaze(theme=COLOR.light)
+a=agent(m, shape = 'arrow', footprints = True)
+a.position = (5,4)
+a.position = (5,3)
+a.position = (5,2)
 m.run()
